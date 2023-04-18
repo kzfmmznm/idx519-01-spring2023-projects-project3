ee99d02bb342ac5d570e7b74faa60d4723611785
Kazufumi Mizunuma <kmizunuma@id.iit.edu>
2023-04-18 11:53:41 -0500

    Update: Update design for map page
    Add heading to the page and centerize components

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
+let map
+
+function initMap() {
+  var opts = {
+    zoom: 15,
+    center: new google.maps.LatLng(35.1239654, 136.9417741),
+  }
+  map = new google.maps.Map(document.getElementById('map'), opts)
+}
+
 window.initMap = initMap
